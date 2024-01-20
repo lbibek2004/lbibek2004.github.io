2161f1a9ddcfda5abe4792a9ce35203f059f2811
Bibek Luitel <146436741+lbibek2004@users.noreply.github.com>
2024-01-20 11:44:48 +0545

    Add files via upload

--- script/personal-web.js
+++ script/personal-web.js
@@ -4,14 +4,3 @@ function closeWindow() {
 function displayPopup() {
     alert("This function is unavailable. Please click on envelope icon under Connect With Me instead.");
 }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownList = document.querySelector(".dropdown-list");
-    var languagesLink = document.querySelector("nav ul li a[href='#']");
-
-    languagesLink.addEventListener("click", function (e) {
-        e.preventDefault();
-        dropdownList.classList.toggle("show");
-    });
-});
