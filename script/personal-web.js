d809c4186775ed565d49c08eaad47640da4b5784
Bibek Luitel <146436741+lbibek2004@users.noreply.github.com>
2024-04-10 08:21:53 +0545

    Add files via upload

--- script/personal-web.js
+++ script/personal-web.js
@@ -1,3 +1,26 @@
 function displayPopup() {
     alert("This function is unavailable. Please click on envelope icon under Connect With Me instead.");
 }
+
+function scrollToHeading(headingId) {
+    var headingElement = document.getElementById(headingId);
+    if (headingElement) {
+        var scrollOffset = headingElement.getBoundingClientRect().top + window.scrollY;
+        var startPosition = window.scrollY;
+        var distance = scrollOffset - startPosition;
+        var duration = 400; // Adjust the duration of the scroll animation (in milliseconds)
+
+        var startTime = null;
+        function scrollToTimestamp(timestamp) {
+            if (!startTime) startTime = timestamp;
+            var progress = timestamp - startTime;
+            var easeInOutQuad = function(t) { return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t; }; // Easing function for smooth animation
+            window.scrollTo(0, startPosition + easeInOutQuad(Math.min(progress / duration, 1)) * distance);
+            if (progress < duration) {
+                window.requestAnimationFrame(scrollToTimestamp);
+            }
+        }
+
+        window.requestAnimationFrame(scrollToTimestamp);
+    }
+}
